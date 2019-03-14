398cbccce658cff1c4d4114e798cf0e97383cd53
Elise Phan <aliceconnic@gmail.com>
2019-03-14 19:13:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ const returnLastTwoDrivers = function (drivers) {
 
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
 
+const createFareMultiplier = function(someInteger){
+  someInteger
+} 
