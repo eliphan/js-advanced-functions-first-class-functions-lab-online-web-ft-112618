18a7a8c84575d0730e24d39e7b0fb0c3269b7602
Elise Phan <aliceconnic@gmail.com>
2019-03-14 19:08:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Code your solution in this file!
 
-  const twoDrivers = function returnFirstTwoDrivers(drivers) {
+  const returnFirstTwoDrivers = function (drivers) {
     return drivers.slice(0,2)
   }
-
-const  = function (drivers) {
-  return drivers.slice(0, 2);
-};
